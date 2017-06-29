b41bdd35adf13c7e379a1a7b0aa9461080e41747
absolutes <15291575887@163.com>
2017-06-29 17:39:09 +0800

    继续安装插件，实现脚手架模块化。

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 
-const [a,b,c] = [1,2,3];
-const d = {a,b,c};
+import React,{Component} from 'react';
+import ReactDom,{render} from 'react-dom';
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <h1>参加React高级课程</h1>
+            </div>
+        )
+    }
+}
+ReactDom.render(<App />,document.getElementById('app'));
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,41 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry:__dirname + '/src/index.js',
+    entry:{index:__dirname + '/src/index.js'},
     output:{
         path:__dirname+'/libs',
-        filename:"[name]-[hash].js"
+        filename:"[name].js"
+        //filename:"index.js"
     },
     module:{
         loaders:[
             {
                 test:/\.js$/,
-                loader:'jsx-loader'
+                loader:['babel-loader']
+            },
+            {
+                test:/\.css/,
+                loader:[
+                    'style!css',
+                    'css-loader?modules&localIdentName=[name]_[local]_[hash:base64:5]',
+                    'postcss-loader'
+                ]
             }
         ]
-    }
+    },
+    devtool:"cheap-module-source-map",
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'webpack demo',
+            template:'./src/index.html'
+        })
+    ]
+    //,devServer:{
+    //    progress:true,
+    //    contentBase:'libs',
+    //    stats:{colors:true},
+    //    inline:true,
+    //    publicPath:'./static/'
+    //}
 }
